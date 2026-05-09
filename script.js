6bee1ad561b8244561ff45958037fb5c118eb68b
ANUJKUMAR7652 <64329462+ANUJKUMAR7652@users.noreply.github.com>
2026-05-09 13:47:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('btn').addEventListener('click', function() {
+    document.getElementById('message').innerText = "App successfully chal raha hai! 🚀";
+    console.log("Button Clicked!");
+});
